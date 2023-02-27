0b5999fc74f6a1a0952c15d5aecb3c7472d90da4
stefan-lemmens <stefanlemmens1977@gmail.com>
2023-02-27 13:21:41 +0100

    Conanfile updated to github repo

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 from conans import ConanFile
 
 class LabVIEWBuild(ConanFile):
-    python_requires = "LabVIEWConanExtension/[~=20.0.*]@intersoft/stable"
+    python_requires = "labview_conan_extension/[~=0.0.*]@user/stable"
     python_requires_extend = "LabVIEWConanExtension.LabVIEWConanExtension"
-    gitURL = "ssh://git@bitbucket.atlassian.inventive-engineering.com:7999/~stefan/labview_conan_poc_libc.git"
+    gitURL = "ssh://git@ssh.github.com:443/stefan-lemmens/labview_conan_poc_libc.git"
