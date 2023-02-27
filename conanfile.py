c3b2e761bb468de3c58dc99c7cda4b21923e21bd
stefan-lemmens <stefanlemmens1977@gmail.com>
2023-02-27 13:22:57 +0100

    Updated python_requires_extend

--- conanfile.py
+++ conanfile.py
@@ -2,5 +2,5 @@ from conans import ConanFile
 
 class LabVIEWBuild(ConanFile):
     python_requires = "labview_conan_extension/[~=0.0.*]@user/stable"
-    python_requires_extend = "LabVIEWConanExtension.LabVIEWConanExtension"
+    python_requires_extend = "labview_conan_extension.LabVIEWConanExtension"
     gitURL = "ssh://git@ssh.github.com:443/stefan-lemmens/labview_conan_poc_libc.git"
